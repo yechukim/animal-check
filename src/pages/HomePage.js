482ae6dfd8f8ff65cef54754e7199751fdd51f83
yechu <yechukim217@gmail.com>
2022-03-13 20:37:42 +0900

    feat: add check human bmi

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import FormWrapper from '../components/FormWrapper';
+import Input from '../components/Input';
 
 function HomePage() {
-  return (
-    <div>
 
-    </div>
+
+
+
+  return (
+    <FormWrapper>
+      애기들 비만도보다 내 비만도를 먼저 확인해보고 싶다면?
+
+    </FormWrapper>
   );
 }
 
